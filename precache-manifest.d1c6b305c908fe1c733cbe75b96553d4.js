42bb3d7bf8a7ed560f289775671bbf5a428696b0
Lilit <lilit@inone.am>
2020-08-03 19:04:29 +0400

    Updates

---
+++ precache-manifest.d1c6b305c908fe1c733cbe75b96553d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19ab530e1ec2abe42f28cb0c54f71ae7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80a765fa8d4ed5512d10",
+    "url": "/static/css/main.782bd57e.chunk.css"
+  },
+  {
+    "revision": "1e113a53aa670e3217e9",
+    "url": "/static/js/2.37fa0545.chunk.js"
+  },
+  {
+    "revision": "3f42448a2329b3598b9d7f2cc7cf1112",
+    "url": "/static/js/2.37fa0545.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80a765fa8d4ed5512d10",
+    "url": "/static/js/main.ee985609.chunk.js"
+  },
+  {
+    "revision": "a27dac9489af7231e3b7",
+    "url": "/static/js/runtime-main.d1e5c7af.js"
+  }
+]);
